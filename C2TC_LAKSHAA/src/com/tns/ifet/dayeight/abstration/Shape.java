46c639aaef08565524601fc4e2ace4eb1f8b48cc
lakshaaRangabashyam <140896051+lakshaaRangabashyam@users.noreply.github.com>
2024-11-12 19:43:25 +0530

    Initial commit

---
+++ C2TC_LAKSHAA/src/com/tns/ifet/dayeight/abstration/Shape.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.dayeight.abstration;
+
+public abstract class Shape {
+	 protected float area;
+	 void show()
+	 {
+		 System.out.println("Area of shape is "+area);
+	 }
+	void calArea() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
