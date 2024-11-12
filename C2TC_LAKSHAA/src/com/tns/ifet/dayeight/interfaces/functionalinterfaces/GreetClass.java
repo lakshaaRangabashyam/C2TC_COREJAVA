f829aca545f401729d6163b272db1e236da9f240
lakshaaRangabashyam <140896051+lakshaaRangabashyam@users.noreply.github.com>
2024-11-12 22:23:07 +0530

    Initial commit

---
+++ C2TC_LAKSHAA/src/com/tns/ifet/dayeight/interfaces/functionalinterfaces/GreetClass.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.dayeight.interfaces.functionalinterfaces;
+
+public class GreetClass implements GreetInterface {
+
+	@Override
+	public String greet() {
+		// TODO Auto-generated method stub
+		return "Welcome to the world of java" ;
+	}
+
+}
---
+++ C2TC_LAKSHAA/src/com/tns/ifet/dayeight/interfaces/functionalinterfaces/GreetInterface.java
@@ -0,0 +1,5 @@
+package com.tns.ifet.dayeight.interfaces.functionalinterfaces;
+@FunctionalInterface
+public interface GreetInterface {
+public String greet();
+}
